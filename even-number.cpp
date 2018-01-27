34f9fdcbbdec29b65a97409438a8202c38014f1f
mittalsaurab <mittalsaurabkumar@gmail.com>
2018-01-28 02:05:19 +0530

    print odd numbers

--- even-number.cpp
+++ even-number.cpp
@@ -5,11 +5,12 @@ int main()
   int i,n,a;
   cout<<"Enter the max value";
   cin>>n;
-  cout<<"Hello everybody";
+  cout<<"Hello everybody!";
   for(i=1;i<=n;i++)
   {
     if(i%2==0)
-    cout<<i;
+    cout<<i-1;
   }
+
   exit (0);
 }
